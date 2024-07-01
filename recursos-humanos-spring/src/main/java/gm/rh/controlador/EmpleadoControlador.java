0554e96f9712701238eb2bb97fb522a26a5ac283
HinaraSM12 <hinarasm01212@gmail.com>
2024-07-01 01:02:00 -0500

    Initial commitAñadiendo proyecto fullstack

---
+++ recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,82 @@
+package gm.rh.controlador;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gm.rh.excepcion.RecursoNoEncontradoExcepcion;
+import gm.rh.modelo.Empleado;
+import gm.rh.servicio.IEmpleadoServicio;
+
+@RestController
+
+// http://localhost:8080/rh-app/
+@RequestMapping("rh-app")
+
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+
+    // http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados() {
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString())));
+        return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id) {
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion(("No se encontró el id: " + id));
+        return ResponseEntity.ok(empleado);
+    }
+
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id,
+            @RequestBody Empleado empleadoRecibido) {
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion("El id recibido no existe: " + id);
+        empleado.setNombre(empleadoRecibido.getNombre());
+        empleado.setDepartamento(empleadoRecibido.getDepartamento());
+        empleado.setSueldo(empleadoRecibido.getSueldo());
+        empleadoServicio.guardarEmpleado(empleado);
+        return ResponseEntity.ok(empleado);
+    }
+
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion("El id recibido no existe: " + id);
+        empleadoServicio.eliminarEmpleado(empleado);
+        //Json {"eliminado": "true"}
+        Map<String, Boolean> respuesta= new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
+    }
+}
